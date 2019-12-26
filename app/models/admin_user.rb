d19681fb3e864a4b0bac0bb745b655ffbaf0b56c
Miguel Bustamante <mbustamante98@hotmail.com>
2019-12-26 16:01:23 -0700

    chpt 6: associations, and join table

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,5 +1,7 @@
 class AdminUser < ApplicationRecord
-
+	has_and_belongs_to_many :pages
+	has_many :section_edits
+	has_many :sections, :through => :section_edits
 	#this is users if the class name was still users and we wanted to use another table
 	#self.table_name  = "admin_users"
 end
